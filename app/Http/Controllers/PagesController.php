089748314d594f55d2b1d6cce0286d8fd75c8ab1
Alejandro Cayssials <admin@webxander.com>
2020-10-06 01:43:44 -0300

    add: Plans views

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Plan;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
@@ -10,6 +11,14 @@ class PagesController extends Controller
     public function index()
     {
         return view('index');
+    }
+
+    public function plans()
+    {
+
+        $plans = Plan::where('role_id', 2)->get();
+        
+        return view('plans', compact('plans'));
     }
 
 }
