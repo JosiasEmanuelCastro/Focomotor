b6d64bd15e07524a065935a05e17175900160671
Alejandro Cayssials <admin@webxander.com>
2020-10-09 19:38:36 -0300

    integration with Mercadopago DEMO

--- resources/views/elements/plan.blade.php
+++ resources/views/elements/plan.blade.php
@@ -9,5 +9,33 @@
             <p class="d-inline-block small py-4">Publicar hasta {{$plan->articles_limit}} clasificados</p>
         </div>
         <p class="btn btn-outline-primary rounded-pill px-4 mb-4 focom-price-btn">Conseguir</p>
+        <form action="/procesar-pago" method="POST">
+            <script
+             src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+             data-preference-id="{{ $plan->preference->id }}">
+            </script>
+        </form>
+
+        
+                    
+<a mp-mode="dftl" href="http://mpago.la/2hquKnY" name="MP-payButton" class='blue-ar-l-rn-none'>Suscribirme</a>
+<script type="text/javascript">
+    (function() {
+        function $MPC_load() {
+            window.$MPC_loaded !== true && (function() {
+                var s = document.createElement("script");
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = document.location.protocol + "//secure.mlstatic.com/mptools/render.js";
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+                window.$MPC_loaded = true;
+            })();
+        }
+        window.$MPC_loaded !== true ? (window.attachEvent ? window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;
+    })();
+</script>
+
+                
     </div>
 </div>
