44053e4fa048185d27f372f4984cc72bab885197
Alejandro Cayssials <admin@webxander.com>
2020-10-16 03:23:58 -0300

    update: some migrations

---
+++ database/migrations/2020_10_15_020808_update_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('location')->nullable();
+            $table->integer('views')->default(0);
+            $table->string('model_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn('location');
+            $table->dropColumn('views');
+        });
+    }
+}
