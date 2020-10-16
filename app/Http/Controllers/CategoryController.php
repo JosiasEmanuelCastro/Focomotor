81940dfacfe8ee19d7aa171fcaa8ff8dd3bf7c26
Alejandro Cayssials <admin@webxander.com>
2020-10-16 03:13:09 -0300

    add: type of category.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,6 +10,20 @@ class CategoryController extends Controller
 
     public function index()
     {
-    	return Category::all();
+    	return Category::where('parent', 0)->get();
+    }
+
+    public function show($category)
+    {
+        $category = Category::find($category);
+
+       
+        return [
+                'items' => $category->subCategories,
+                'children_node' => $category->type->name, 
+            ];
+
+         
+       
     }
 }
